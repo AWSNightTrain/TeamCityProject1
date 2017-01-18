d73fea2703a53121347529b54d8823195b692dfe
Alain Nacario <alain.nacario@ptml.com>
2017-01-19 12:17:29 +1300

    Project1 and Test

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        private static string VersionNo = "0.01";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Format("verison: {0}", VersionNo));
+        }
+    }
+}
